1d0ce1224788bb74990cd8e8473c1aa16cb27730
lalaorujzada-maker <lalaorujzada@gmail.com>
2026-04-13 20:03:25 +0400

    Update SecondExamService.java

--- src/main/java/com/student/mentorproject/service/SecondExamService.java
+++ src/main/java/com/student/mentorproject/service/SecondExamService.java
@@ -6,19 +6,15 @@ import com.student.mentorproject.dto.response.SecondExamResponseDto;
 import java.util.List;
 
 public interface SecondExamService {
-    // CREATE (REGISTER)
     SecondExamResponseDto register(SecondExamRequestDto requestDto);
 
-    // READ
     SecondExamResponseDto getById(Long id);
 
     SecondExamResponseDto getByStudentId(Long studentId);
 
     List<SecondExamResponseDto> getAll();
 
-    // UPDATE
     SecondExamResponseDto update(Long id, SecondExamRequestDto requestDto);
 
-    // DELETE
     void delete(Long id);
 }
